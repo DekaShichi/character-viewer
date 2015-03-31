eefebbc29eccffaa46f35d59005c365b770349d0
DekaShichi <dekashichi@live.com>
2015-03-31 15:32:23 -0700

    Separated render function in its own file.

--- viewer.js
+++ viewer.js
@@ -210,22 +210,6 @@ function render_full(imageUrl) {
     document.getElementById("full").appendChild(img);
 }
 
-// Renders the info box of the passed in character.
-function render(char) {
-    // Empty the children first.
-    $("#info").empty();
-    
-    addProperty("Name",char.name);
-    addProperty("Age",char.age);
-    // If 'spells' exists add property.
-    if('spells' in char) {
-        var desc = "";
-        if(char.spells.desc) desc = char.spells.desc;
-        var prop = addProperty("Spells", desc);
-        prop.appendChild(addUnorderedList(char.spells.list));
-    }
-}
-
 /* Use to add a field to the info box (e.g. Name: Robert)
  * Pass in the name/label and the text (the text is optional).
  * Example:
