5d55eb3ac71e63aa2c70bd0a4fb09e1cc7a9a02d
DekaShichi <dekashichi@live.com>
2015-03-31 15:14:10 -0700

    Finalized imagebox feature.

--- viewer.js
+++ viewer.js
@@ -3,6 +3,7 @@
 var chars;
 // Used to keep track of the current selected character.
 var currentIndex = -1;
+var currentFullIndex = -1;
 // Used to mitigate a bug where fast toggles will render blank.
 var loading = false;
 $(document).ready(function() {
@@ -49,7 +50,7 @@ function render_grid() {
         // Create a <img> DOM object.
         var img = document.createElement("IMG");
         // Set id as 'charImg0', 'charImg1', etc.
-        img.setAttribute("id","charImg" + i);
+        img.setAttribute("id","char" + i + "Icon");
         // Set src to the icon link.
         img.setAttribute("src",chars[i].icon);
         // Set the alt text as character name.
@@ -68,6 +69,8 @@ function render_grid() {
             if(loading) return;
             // Reset all of the icon borders no matter what.
             $("#grid img").css("border","");
+            // Empty the full image no matter what.
+            $("#full").empty();
             // If this character is already selected.
             if(currentIndex == e.data.index) {
                 // Set the index to an arbitrary 'invalid' number so that when
@@ -89,8 +92,9 @@ function render_grid() {
                 currentIndex = e.data.index;
                 // Render the info box.
                 render(chars[e.data.index]);
+                render_images(e.data.index);
                 // Assigned for convenience & readability.
-                var style = document.getElementById("charImg" + e.data.index)
+                var style = document.getElementById("char" + e.data.index + "Icon")
                         .style;
                 // If the character data has a color.
                 if(chars[e.data.index].hasOwnProperty('color')) {
@@ -107,12 +111,103 @@ function render_grid() {
                 // rendering info.
                 loading = true;
                 // Animate fade in, then set loading to false once complete.
-                $("#info").fadeIn(function() {
-                    $("#images").fadeIn(function() { loading = false;});
+                $("#info").fadeIn(function () {
+                    $("#images").fadeIn(function() { loading = false; });
                 });
             }
         });
     }
+}
+
+// Renders the face icon grid that's clickable. A click will either produce a
+// a full image, or "destroy" it if it's already selected.
+function render_images(index) {
+    // Empty the children first (in case of using load/init again).
+    $("#images").empty();
+    var char = chars[index];
+    if(!char.hasOwnProperty('images') ||
+            !char.images.hasOwnProperty('length')) return;
+    
+    // Loop through each of the characters.
+    for(var i = 0; i < char.images.length; i++) {
+        // Create a <div> DOM object.
+        var div = document.createElement("DIV");
+        // Set id as 'char0', 'char1', etc.
+        div.setAttribute("id","char" + index + "Icon" + i + "Container");
+        
+        // Create a <img> DOM object.
+        var img = document.createElement("IMG");
+        // Set id as 'char0Icon0', 'char0Icon1', etc.
+        img.setAttribute("id","char" + index + "Icon" + i);
+        // Set src to the icon link.
+        img.setAttribute("src",char.images[i].icon);
+        // Set the alt text as [name] Icon [i]
+        img.setAttribute("alt",char.name + " Icon " + i);
+        // If the image is labeled as mature, produce a red secondary border.
+        if(char.images[i].mature) {
+            img.style.outline = "thick solid #c11b17";
+        }
+        
+        // Append the img tag as a child to the div container.
+        div.appendChild(img);
+        // Appent the div container to the #grid element.
+        document.getElementById("images").appendChild(div);
+        // Sets each div under #grid's click event.
+        /* .eq(i) sets the the indexed DOM, .on("cluck", event data, event)
+         * attaches the click event, with the attached event metadata.
+         * In this case, the character index is attached.
+         */
+        $("#images div").eq(i).on("click", { index: i }, function(e) {
+            if(loading) return;
+            // Reset all of the icon borders no matter what.
+            $("#images img").css("border","");
+            
+            // If this character is already selected.
+            if(currentFullIndex == e.data.index) {
+                // Set the index to an arbitrary 'invalid' number so that when
+                // selected again it's never initially true.
+                currentFullIndex = -1;
+                // Empty data once done fading out only if it's not loading
+                // new data, otherwise will produce a bug that will wipe out
+                // the info that's being rendered elsewhere.
+                $("#full").empty();
+            }
+            // Else it's not already selected.
+            else
+            {
+                // Set the currently selected character index.
+                currentFullIndex = e.data.index;
+                render_full(char.images[e.data.index].full);
+                // Assigned for convenience & readability.
+                var icon = $("#char" + index + "Icon" + e.data.index);
+                // If the image data has a color.
+                if(char.images[e.data.index].hasOwnProperty('color')) {
+                    icon.css('border',"thick solid " + char.images[e.data.index].color);
+                }
+                else {
+                    // If the character data has a color.
+                    if(char.hasOwnProperty('color')) {
+                        icon.css('border',"thick solid " + char.color);
+                    }
+                    // If not, use a default color.
+                    else {
+                        icon.css('border',"thick solid turquoise");
+                    }
+                }
+            }
+        });
+    }
+}
+
+function render_full(imageUrl) {
+    $("#full").empty();
+    
+    // Creates containers for each field.
+    var img = document.createElement("IMG");
+    img.setAttribute("id","fullImg");
+    img.setAttribute("src",imageUrl);
+    // Append the container to the info box.
+    document.getElementById("full").appendChild(img);
 }
 
 // Renders the info box of the passed in character.
