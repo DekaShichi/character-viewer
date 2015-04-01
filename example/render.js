82c1cd363d765a695d35c84be9209f1738cc04b4
DekaShichi <dekashichi@live.com>
2015-03-31 23:32:25 -0700

    Set up example folder.

---
+++ example/render.js
@@ -0,0 +1,15 @@
+// Renders the info box of the passed in character.
+function render(char) {
+    // Empty the children first.
+    $("#info").empty();
+    
+    addProperty("Name",char.name);
+    addProperty("Age",char.age);
+    // If 'spells' exists add property.
+    if('spells' in char) {
+        var desc = "";
+        if(char.spells.desc) desc = char.spells.desc;
+        var prop = addProperty("Spells", desc);
+        prop.appendChild(addUnorderedList(char.spells.list));
+    }
+}
