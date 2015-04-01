59b37bb88a31f92b1f55bb1ca92e68725877ae0f
DekaShichi <dekashichi@live.com>
2015-04-01 00:16:04 -0700

    Updated example render comments.

--- example/render.js
+++ example/render.js
@@ -1,4 +1,8 @@
 // Renders the info box of the passed in character.
+/* Override this by using the addProperty, addOrderedList, and/or
+ * addUnorderedList functions. Generally you should add (appendChild())
+ * a list to a returned Property (a div), or #info itself.
+ */
 function render(char) {
     addProperty("Name",char.name);
     addProperty("Age",char.age);
